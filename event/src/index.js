7ece6cafc4448f7b943eb02a328aa485bfc5725b
Muskan Maheshwari <56144289+Muskan02@users.noreply.github.com>
2021-06-19 21:23:48 +0530

    deployed

--- event/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-require('dotenv').config();
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
